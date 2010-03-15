5ddee7bef1daf43e0bb8a5ea4343058b4091dce7
Caleb Claggett <caleb@widget.localdomain>
2010-03-15 15:36:31 -0400

    Added Group constraint. Added unit tests. Reordered file.

--- constraint2.py
+++ constraint2.py
@@ -1,11 +1,42 @@
-from functools import partial
-
 # Verdict Flags.
 Invalid   = 0 # 00 # Not matching and don't continue.
 Continue  = 1 # 01 # Not matching but continue.
-Matching  = 2 # 10 # Matching and don't continue.
+Matching  = 2 # 10 # Matching but don't continue.
 Satisfied = 3 # 11 # Matching and continue.
 
+# Helper class to create mutable state.
+class Bunch(dict):
+    def __init__(self, **kwds):
+        self.update(kwds)
+        self.__dict__ = self
+    __call__ = __init__
+
+# Utility functions that work with constraints.
+def match(constraint, tokens):
+    'Compare constraint against a list of tokens.'
+    apply, verdict = instance(constraint) 
+
+    for token in tokens:
+        if not verdict & Continue:
+            # The previous verdict indicated no continue so this
+            # token stream can never match.
+            verdict = Invalid
+            break
+
+        verdict = apply(token)
+
+    return bool(verdict & Matching)
+
+def instance(constraint):
+    'Create a new instance of the constraint and manage/hide the state.'
+    state, verdict = constraint.init()
+    wrapped = Bunch(state=state)
+    def wrapper(token):
+        wrapped.state, verdict = constraint.apply(wrapped.state, token)
+        return verdict
+    return wrapper, verdict
+
+# Library of constraints.
 def Null():
     'Matches nothing.'
     def init():
@@ -42,8 +73,7 @@ def Range(min=0, max=None):
     'Matches count tokens where: min <= count <= max.'
     def init():
         state = Bunch(count=-1)
-        verdict = apply(None)
-        return state, verdict
+        return apply(state, None)
 
     def apply(state, token):
         state.count += 1
@@ -51,9 +81,9 @@ def Range(min=0, max=None):
         if state.count < min:
             verdict = Continue
         elif max != None:
-            if self.count == max:
+            if state.count == max:
                 verdict = Matching
-            elif self.count > max:
+            elif state.count > max:
                 verdict = Invalid
         return state, verdict
 
@@ -89,16 +119,14 @@ def Single():
     return Bunch(init=init, apply=apply)
 
 def And(*constraints):
-    'Apply all constraints at the same time.'
+    'Matches all constraints.'
     def init():
         state = []
-        final_verdict = Satisfied
-        for constraint in constraints:
-            # Wrap constraints so their states are handled cleanly.
-            apply, verdict = wrap(constraint)
+        verdict = Satisfied
+        for apply, subverdict in map(instance, constraints):
             state.append(apply)
-            final_verdict &= verdict
-        return state, final_verdict
+            verdict &= subverdict
+        return state, verdict
 
     def apply(state, token):
         verdict = Satisfied
@@ -108,50 +136,73 @@ def And(*constraints):
 
     return Bunch(init=init, apply=apply)
 
-def Sequence(*constraints):
-    'Apply each constraint serially.'
-    pass
+def Or(*constraints):
+    'Matches at least one constraint.'
+    def init():
+        state = []
+        verdict = Invalid
+        for apply, subverdict in map(instance, constraints):
+            state.append(apply)
+            verdict |= subverdict
+        return state, verdict
 
-def Parallel(*constraints):
-    'Apply any and all constraints.'
-    pass
+    def apply(state, token):
+        verdict = Invalid
+        for apply in state:
+            verdict |= apply(token)
+        return state, verdict
+
+    return Bunch(init=init, apply=apply)
+
+def Group(*constraints, **kwds):
+    'Matches all constraints in any order limited by pconstraint.'
+    pconstraint = kwds.get('constraint', Any())
+
+    def init():
+        pstate, pverdict = pconstraint.init()
+        return [ (pstate, pverdict, None, None, Matching) ], pverdict
+
+    def apply(paths, token):
+        # Calculate the new state (the list of candidate paths).
+        new_paths = []
+        for pstate, pverdict, id, apply, verdict in paths:
+
+            if verdict & Continue:
+                # Apply the token to this path
+                new_verdict = apply(token)
+
+                if new_verdict != Invalid:
+                    new_paths.append((pstate, pverdict, id, apply, new_verdict))
+
+            if verdict & Matching:
+                # Search for new paths
+                for new_id, constraint in enumerate(constraints):
+                    new_pstate, new_pverdict = pconstraint.apply(pstate, new_id)
+                    if new_pverdict != Invalid:
+                        new_apply, initial_verdict = instance(constraint)
+                        if initial_verdict & Continue:
+                            new_verdict = new_apply(token)
+                            if new_verdict != Invalid:
+                                new_paths.append((new_pstate, new_pverdict,
+                                    new_id, new_apply, new_verdict))
+
+        # Calculate the verdict as the logical union of all path verdicts.
+        final_verdict = Invalid
+        for pstate, pverdict, id, apply, verdict in new_paths:
+            final_verdict |= pverdict
+
+        print 'old %d -> new %d' % (len(paths), len(new_paths))
+        return new_paths, final_verdict
+
+    return Bunch(init=init, apply=apply)
 
 def Attribute(name, constraint):
     'Apply constraint to the named token attribute.'
     def init():
-        return wrap(constraint)
+        return instance(constraint)
     def apply(state, token):
         return state, state(getattr(token, name))
     return Bunch(init=init, apply=apply)
-
-def match(constraint, tokens):
-    apply, verdict = wrap(constraint) 
-
-    for token in tokens:
-        if not verdict & Continue:
-            # The previous verdict indicated no continue so this
-            # token stream can never match.
-            verdict = Invalid
-            break
-
-        verdict = apply(token)
-
-    return bool(verdict & Matching)
-
-def wrap(constraint):
-    state, verdict = constraint.init()
-    wrapped = Bunch(state=state)
-    def wrapper(token):
-        wrapped.state, verdict = constraint.apply(wrapped.state, token)
-        return verdict
-    return wrapper, verdict
-
-# Helper class to create mutable state.
-class Bunch(dict):
-    def __init__(self, **kwds):
-        self.update(kwds)
-        self.__dict__ = self
-    __call__ = __init__
 
 # Unit Testing
 import unittest
@@ -162,7 +213,9 @@ class TestBasic(unittest.TestCase):
         pass
 
     def match(self, constraint, tokens):
-        return self.assertTrue(match(constraint, tokens))
+        return self.assertTrue(
+            match(constraint, tokens)
+            )
 
     def nomatch(self, constraint, tokens):
         return self.assertFalse(match(constraint, tokens))
@@ -182,6 +235,70 @@ class TestBasic(unittest.TestCase):
         nine, ten = range(9), range(10)
         self.match(Member(ten), nine)
         self.nomatch(Member(nine), ten)
+
+    def testMemberRange(self):
+        c = MemberRange(1,6)
+        self.match(c, [1,2,3,4,5,6,1,2,3,4,5,6])
+        self.nomatch(c, [0])
+        self.nomatch(c, [7])
+
+    def testRange(self):
+        c = Range(min=1,max=3)
+        self.nomatch(c, [])
+        self.match(c, [1])
+        self.match(c, '11')
+        self.match(c, '111')
+        self.nomatch(c, '1111')
+
+        c = Range(min=2,max=None)
+        self.nomatch(c, '')
+        self.nomatch(c, '1')
+        self.match(c, '11')
+        self.match(c, '111')
+        self.match(c, '1111')
+
+    def testAnd(self):
+        c = And(Null())
+        self.match(c, [])
+        self.nomatch(c, [1])
+
+        c = And(Any(), Any())
+        self.match(c, [])
+        self.match(c, [1])
+        self.match(c, [1,1])
+
+        c = And(Null(), Any())
+        self.match(c, [])
+        self.nomatch(c, [1])
+        self.nomatch(c, [1,1])
+
+        c = And(Range(1,2), Member('abc'))
+        self.match(c, 'a')
+        self.match(c, 'bc')
+        self.nomatch(c, 'abc')
+        self.nomatch(c, '')
+
+    def testOr(self):
+        c = Or(Range(1,1), Range(3,4))
+        self.nomatch(c, '')
+        self.match(c, 'a')
+        self.nomatch(c, 'ab')
+        self.match(c, 'abc')
+        self.match(c, 'abcd')
+        self.nomatch(c, 'abcde')
+
+    def testGroup(self):
+        digits = MemberRange('0','9')
+        dashes = Member('-')
+
+        digit = And(Single(), digits)
+        dash = And(Single(), dashes)
+
+        areacode = And(Range(min=3, max=4), digits)
+        self.match(areacode, '123')
+
+        phone = Group(areacode, dashes)
+        self.match(phone, '123-456-7890')
 
     def testAttribute(self):
         tokens = [Bunch(x=1,y=2,z=3) for i in range(2)]
